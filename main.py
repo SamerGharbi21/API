1cab4a4a0cbe3b860f7a77dd1da0757989afb314
SamerGharbi21 <Gharbisamer2@gmail.com>
2024-08-29 00:26:47 +0300

    alhumd llah

--- main.py
+++ main.py
@@ -6,42 +6,12 @@ model = joblib.load('reg.joblib')
 scaler = joblib.load('scaler.joblib')
 app = FastAPI()
 
-
-
-
 # GET request
 @app.get("/")
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-
-
 
 def read_root():
     return {"message": "Welcome to Tuwaiq Academy"}
 # get request
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-
-
-
-
 
 @app.get("/try/{item_id}")
 async def read_item(item_id):
@@ -50,21 +20,6 @@ async def read_item(item_id):
 class InputFeatures(BaseModel):
     appearance: int
     highest_value: int 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def preprocessing(input_features: InputFeatures):
@@ -76,31 +31,9 @@ def preprocessing(input_features: InputFeatures):
     return scaler.transform([list(dict_f.values())])
 
 
-
-
-
-
-
-
-
-
-
-
-
 @app.get("/predict")
 def predict(input_features: InputFeatures):
     return preprocessing(input_features)
-
-
-
-
-
-
-
-
-
-
-
 
 @app.post("/predict")
 async def predict(input_features: InputFeatures):
